f2333a2aceefa526f23facb3beb2b9376e3a8d00
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-09-08 13:22:30 -0700

    Basic structure
    Basic structure of spaceman()

--- spaceman.py
+++ spaceman.py
@@ -14,7 +14,7 @@ def load_word():
 
     words_list = words_list[0].split(' ') #comment this line out if you use a words.txt file with each word on a new line
     secret_word = random.choice(words_list)
-    return secret_word
+    return secret_word.upper()
 
 def is_word_guessed(secret_word, letters_guessed):
     '''
@@ -48,35 +48,46 @@ def get_guessed_word(secret_word, letters_guessed):
 
 
 def is_guess_in_word(guess, secret_word):
-    '''
-    A function to check if the guessed letter is in the secret word
-
-    Args:
-        guess (string): The letter the player guessed this round
-        secret_word (string): The secret word
-
-    Returns:
-        bool: True if the guess is in the secret_word, False otherwise
-
-    '''
-    #TODO: check if the letter guess is in the secret word
-
+    return guess in secret_word
     pass
 
 
+def page_break():
+    print("------------------------------------")
+
+
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
+
+
+def get_guess(prompt):
+    letter = user_input(prompt).upper().strip()
+    if len(letter) != 1:
+        return get_guess("Please enter exactly one letter: ")
+    elif already_guessed(letter)
+        return get_guess("Please guess a new letter: ")
+    else:
+        return letter
 
 
 def spaceman(secret_word):
-    '''
-    A function that controls the game of spaceman. Will start spaceman in the command line.
+    length = len(secret_word)
+    print("Help spaceman get to space by correctly guessing letters in the launch code!\nBe careful though, you only get 7 incorrect guesses before the rocket falls apart."):
+    print("The launch code has: " + length + "letters")
+    page_break()
+    print(secret_word)
+    while is_word_guessed()==False:
+        print(get_guessed_word)
+        guess = get_guess("Enter a letter: ")
+        if is_guess_in_word(guess):
+            print("That letter is in the launch code!")
+        else:
+            print("Oops! Your rocket just lost a piece.")
+        page_break()
+    
 
-    Args:
-      secret_word (string): the secret word to guess.
 
-    '''
-
-
-    #TODO: show the player information about the game according to the project spec
 
     #TODO: Ask the player to guess one letter per round and check that it is only one letter
 
@@ -85,7 +96,6 @@ def spaceman(secret_word):
     #TODO: show the guessed word so far
 
     #TODO: check if the game has been won or lost
-
 
 
 
