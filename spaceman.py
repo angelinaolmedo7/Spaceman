52c1d7ea8758a9fd1e5dd6cd8981808e29da50cc
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-09-08 13:51:23 -0700

    Win condition
    Player wins and program ends if all letters are guessed.

--- spaceman.py
+++ spaceman.py
@@ -17,21 +17,10 @@ def load_word():
     return secret_word.upper()
 
 def is_word_guessed(secret_word, letters_guessed):
-    '''
-    A function that checks if all the letters of the secret word have been guessed.
-
-    Args:
-        secret_word (string): the random word the user is trying to guess.
-        letters_guessed (list of strings): list of letters that have been guessed so far.
-
-    Returns:
-        bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
-    '''
-
-    return False
-
-    # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
-    pass
+    for elem in secret_word:
+        if elem not in letters_guessed:
+            return False
+    return True
 
 def get_guessed_word(secret_word, letters_guessed):
     rtn = ""
@@ -84,7 +73,7 @@ def spaceman(secret_word):
         else:
             print("Oops! Your rocket just lost a piece.")
         page_break()
-
+    print ("Wow! You won. The launch code was " + secret_word)
     #TODO: show the guessed word so far
 
     #TODO: check if the game has been won or lost
