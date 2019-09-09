77ad20c15a7f8996caaaa257c7553d5f87317ffc
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-09-09 12:38:41 -0700

    ASCII Art
    Spaceship art that reflects incorrect guesses

--- spaceman.py
+++ spaceman.py
@@ -57,34 +57,168 @@ def get_guess(prompt, guess_list):
         return letter
 
 
+def color(item, colr):
+    if colr == "red":
+        return "\033[31m{}\033[00m".format(item)
+    elif colr == "orange":
+        return "\033[91m{}\033[00m".format(item)
+    elif colr == "yellow":
+        return "\033[93m{}\033[00m".format(item)
+    elif colr == "green":
+        return "\033[32m{}\033[00m".format(item)
+    elif colr == "blue":
+        return "\033[34m{}\033[00m".format(item)
+    elif colr == "purple":
+        return "\033[35m{}\033[00m".format(item)
+    elif colr == "pink":
+        return "\033[95m{}\033[00m".format(item)
+    elif colr == "indigo":
+        return "\033[94m{}\033[00m".format(item)
+    elif colr == "rainbow":
+        color_count = 0
+        new_string = ""
+        for elem in item:
+            if elem == " ":
+                new_string += elem
+                color_count += -1
+            elif color_count == 0:
+                new_string += color(elem, "red")
+            elif color_count == 1:
+                new_string += color(elem, "orange")
+            elif color_count == 2:
+                new_string += color(elem, "yellow")
+            elif color_count == 3:
+                new_string += color(elem, "green")
+            elif color_count == 4:
+                new_string += color(elem, "blue")
+            elif color_count == 5:
+                new_string += color(elem, "purple")
+            elif color_count == 6:
+                new_string += color(elem, "pink")
+                color_count = -1
+            color_count += 1
+        return new_string
+    elif colr == "rainbow sp":
+        color_count = 0
+        new_string = ""
+        for elem in item:
+            if color_count == 0:
+                new_string += color(elem, "red")
+            elif color_count == 1:
+                new_string += color(elem, "orange")
+            elif color_count == 2:
+                new_string += color(elem, "yellow")
+            elif color_count == 3:
+                new_string += color(elem, "green")
+            elif color_count == 4:
+                new_string += color(elem, "blue")
+            elif color_count == 5:
+                new_string += color(elem, "purple")
+            elif color_count == 6:
+                new_string += color(elem, "pink")
+                color_count = -1
+            color_count += 1
+        return new_string
+    else:
+        return item
+
+
+def print_ship(count, word_line):
+    if count == -1:
+        print("                       /\\")
+        print("                      /  \\")
+        print("                     / /\\ \\")
+        print("                    | (__) |")
+        print("                    |  " + color("o","green") + "   |")
+        print("                    |   " + color("o","green") + "  |")
+        print("                    |  " + color("o","green") + "   |")
+        print("                    |   " + color("o","green") + "  |")
+        print("                   /|  " + color("o","green") + "   |\\")
+        print("                  / |   " + color("o","green") + "  | \\")
+        print("                 |  |  " + color("o","green") + "   |  |")
+        print("                 | /|______|\\ |")
+        print("                 |/  " + color("((()))","orange") + "  \\|")
+        print(color("                    (((|)))","orange"))
+        print(color("                     (((|)))","orange"))
+        print(color("                    (((|)))","orange"))
+        print(color("                     (((|)))","orange"))
+        print(color("                     ((()))","orange"))
+        print(color("                       ()","orange"))
+        print("")
+        print(color(" __     __  _____  ______   ___    _____  _____","rainbow"))
+        print(color("|  |   |  ||  ___||__  __|/  _  \\ |  ___||  ___|","rainbow"))
+        print(color("|  |__ |  ||  __|   |  | |  | |  ||  __| |  __|","rainbow"))
+        print(color("|_____||__||__|     |__|  \\ ___ / |__|   |__|","rainbow"))
+    elif count < -1 or count > 7:
+        print(color("Something's gone wrong! Please restart the game.", "red")) #this shouldn't happen
+    else:
+        print("      /\\")
+        print("     /  \\")
+        print("    / /\\ \\")
+        print("   | (__) |")
+        if count > 0:
+            print("   |  " + color("o","red") + "   |")
+        else:
+            print("   |  o   |")
+
+        if count > 1:
+            print("   |   " + color("o","red") + "  |  ACCESS CODE: " + word_line)
+        else:
+            print("   |   o  |  ACCESS CODE: " + word_line)
+
+        if count > 2:
+            print("   |  " + color("o","red") + "   |  ATTEMPTS REMAINING: " + str(7-count) + "/7")
+        else:
+            print("   |  o   |  ATTEMPTS REMAINING: " + str(7-count) + "/7")
+
+        if count > 3:
+            print("   |   " + color("o","red") + "  |")
+        else:
+            print("   |   o  |")
+
+        if count > 4:
+            print("  /|  " + color("o","red") + "   |\\")
+        else:
+            print("  /|  o   |\\")
+
+        if count > 5:
+            print(" / |   " + color("o","red") + "  | \\")
+        else:
+            print(" / |   o  | \\")
+
+        if count > 6:
+            print("|  |  " + color("o","red") + "   |  |")
+        else:
+            print("|  |  o   |  |")
+
+        print("| /|______|\\ |")
+        print("|/    \\/    \\|")
+
+
 def spaceman(secret_word):
     length = len(secret_word)
     guess_list = []
     wrong_count = 0
-    print("Help spaceman get to space by correctly guessing letters in the launch code!\nBe careful though, you only get 7 incorrect guesses before the rocket falls apart.")
+    print("Get to space by correctly guessing letters in the rocket's launch code!\nBe careful though, you only get " + color("7","red") + " incorrect guesses before you're locked out.")
     print("The launch code has: " + str(length) + " letters.")
     page_break()
     print(secret_word)
     while is_word_guessed(secret_word, guess_list)==False and wrong_count < 7:
-        print(get_guessed_word(secret_word, guess_list) + " | Incorrect guesses: " + str(wrong_count) + "/7")
+        print_ship(wrong_count, get_guessed_word(secret_word, guess_list))
         guess = get_guess("Enter a letter: ", guess_list)
         guess_list.append(guess)
         if is_guess_in_word(guess, secret_word):
-            print("That letter is in the launch code!")
+            print(color("That letter is in the launch code!", "green"))
         else:
-            print("Oops! Your rocket just lost a piece.")
+            print(color("Oops! That letter is not in the launch code.", "red"))
             wrong_count += 1
         page_break()
     if wrong_count >= 7:
-        print ("Dang. You lost. The launch code was " + secret_word)
+        print_ship(7, get_guessed_word(secret_word, guess_list))
+        print(color("ACCESS DENIED. The code was " + secret_word, "red"))
     else:
-        print ("Wow! You won. The launch code was " + secret_word)
-    #TODO: show the guessed word so far
-
-    #TODO: check if the game has been won or lost
-
-
-
+        print(color("ACCESS GRANTED. The code was " + secret_word, "green"))
+        print_ship(-1, "")
 
 
 #These function calls that will start the game
