f964fedfede4c8ec122e164fc2d612db92e0da6f
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-09-09 13:04:53 -0700

    Alphabet display
    Alphabet is shown beside the rocket, with green letters for correct guesses and red for incorrect

--- spaceman.py
+++ spaceman.py
@@ -22,14 +22,15 @@ def is_word_guessed(secret_word, letters_guessed):
             return False
     return True
 
+
 def get_guessed_word(secret_word, letters_guessed):
-    rtn = ""
+    rtn = ''
     for elem in secret_word:
         if elem in letters_guessed:
             rtn += elem
         else:
-            rtn += "_"
-        rtn += " "
+            rtn += '_'
+        rtn += ' '
     return rtn.strip()
 
 
@@ -39,7 +40,7 @@ def is_guess_in_word(guess, secret_word):
 
 
 def page_break():
-    print("------------------------------------")
+    print('------------------------------------')
 
 
 def user_input(prompt):
@@ -50,72 +51,72 @@ def user_input(prompt):
 def get_guess(prompt, guess_list):
     letter = user_input(prompt).upper().strip()
     if len(letter) != 1:
-        return get_guess("Please enter exactly one letter: ", guess_list)
+        return get_guess('Please enter exactly one letter: ', guess_list)
     elif letter in guess_list:
-        return get_guess("Please guess a new letter: ", guess_list)
+        return get_guess('Please guess a new letter: ', guess_list)
     else:
         return letter
 
 
 def color(item, colr):
-    if colr == "red":
-        return "\033[31m{}\033[00m".format(item)
-    elif colr == "orange":
-        return "\033[91m{}\033[00m".format(item)
-    elif colr == "yellow":
-        return "\033[93m{}\033[00m".format(item)
-    elif colr == "green":
-        return "\033[32m{}\033[00m".format(item)
-    elif colr == "blue":
-        return "\033[34m{}\033[00m".format(item)
-    elif colr == "purple":
-        return "\033[35m{}\033[00m".format(item)
-    elif colr == "pink":
-        return "\033[95m{}\033[00m".format(item)
-    elif colr == "indigo":
-        return "\033[94m{}\033[00m".format(item)
-    elif colr == "rainbow":
+    if colr == 'red':
+        return '\033[31m{}\033[00m'.format(item)
+    elif colr == 'orange':
+        return '\033[91m{}\033[00m'.format(item)
+    elif colr == 'yellow':
+        return '\033[93m{}\033[00m'.format(item)
+    elif colr == 'green':
+        return '\033[32m{}\033[00m'.format(item)
+    elif colr == 'blue':
+        return '\033[34m{}\033[00m'.format(item)
+    elif colr == 'purple':
+        return '\033[35m{}\033[00m'.format(item)
+    elif colr == 'pink':
+        return '\033[95m{}\033[00m'.format(item)
+    elif colr == 'indigo':
+        return '\033[94m{}\033[00m'.format(item)
+    elif colr == 'rainbow':
         color_count = 0
-        new_string = ""
+        new_string = ''
         for elem in item:
-            if elem == " ":
+            if elem == ' ':
                 new_string += elem
                 color_count += -1
             elif color_count == 0:
-                new_string += color(elem, "red")
+                new_string += color(elem, 'red')
             elif color_count == 1:
-                new_string += color(elem, "orange")
+                new_string += color(elem, 'orange')
             elif color_count == 2:
-                new_string += color(elem, "yellow")
+                new_string += color(elem, 'yellow')
             elif color_count == 3:
-                new_string += color(elem, "green")
+                new_string += color(elem, 'green')
             elif color_count == 4:
-                new_string += color(elem, "blue")
+                new_string += color(elem, 'blue')
             elif color_count == 5:
-                new_string += color(elem, "purple")
+                new_string += color(elem, 'purple')
             elif color_count == 6:
-                new_string += color(elem, "pink")
+                new_string += color(elem, 'pink')
                 color_count = -1
             color_count += 1
         return new_string
-    elif colr == "rainbow sp":
+    elif colr == 'rainbow sp':
         color_count = 0
-        new_string = ""
+        new_string = ''
         for elem in item:
             if color_count == 0:
-                new_string += color(elem, "red")
+                new_string += color(elem, 'red')
             elif color_count == 1:
-                new_string += color(elem, "orange")
+                new_string += color(elem, 'orange')
             elif color_count == 2:
-                new_string += color(elem, "yellow")
+                new_string += color(elem, 'yellow')
             elif color_count == 3:
-                new_string += color(elem, "green")
+                new_string += color(elem, 'green')
             elif color_count == 4:
-                new_string += color(elem, "blue")
+                new_string += color(elem, 'blue')
             elif color_count == 5:
-                new_string += color(elem, "purple")
+                new_string += color(elem, 'purple')
             elif color_count == 6:
-                new_string += color(elem, "pink")
+                new_string += color(elem, 'pink')
                 color_count = -1
             color_count += 1
         return new_string
@@ -123,102 +124,123 @@ def color(item, colr):
         return item
 
 
-def print_ship(count, word_line):
+def print_ship(count, word_line, alpha_list):
     if count == -1:
-        print("                       /\\")
-        print("                      /  \\")
-        print("                     / /\\ \\")
-        print("                    | (__) |")
-        print("                    |  " + color("o","green") + "   |")
-        print("                    |   " + color("o","green") + "  |")
-        print("                    |  " + color("o","green") + "   |")
-        print("                    |   " + color("o","green") + "  |")
-        print("                   /|  " + color("o","green") + "   |\\")
-        print("                  / |   " + color("o","green") + "  | \\")
-        print("                 |  |  " + color("o","green") + "   |  |")
-        print("                 | /|______|\\ |")
-        print("                 |/  " + color("((()))","orange") + "  \\|")
-        print(color("                    (((|)))","orange"))
-        print(color("                     (((|)))","orange"))
-        print(color("                    (((|)))","orange"))
-        print(color("                     (((|)))","orange"))
-        print(color("                     ((()))","orange"))
-        print(color("                       ()","orange"))
-        print("")
-        print(color(" __     __  _____  ______   ___    _____  _____","rainbow"))
-        print(color("|  |   |  ||  ___||__  __|/  _  \\ |  ___||  ___|","rainbow"))
-        print(color("|  |__ |  ||  __|   |  | |  | |  ||  __| |  __|","rainbow"))
-        print(color("|_____||__||__|     |__|  \\ ___ / |__|   |__|","rainbow"))
+        print('                       /\\')
+        print('                      /  \\')
+        print('                     / /\\ \\')
+        print('                    | (__) |')
+        print('                    |  ' + color('o','green') + '   |')
+        print('                    |   ' + color('o','green') + '  |')
+        print('                    |  ' + color('o','green') + '   |')
+        print('                    |   ' + color('o','green') + '  |')
+        print('                   /|  ' + color('o','green') + '   |\\')
+        print('                  / |   ' + color('o','green') + '  | \\')
+        print('                 |  |  ' + color('o','green') + '   |  |')
+        print('                 | /|______|\\ |')
+        print('                 |/  ' + color('((()))','orange') + '  \\|')
+        print(color('                    (((|)))','orange'))
+        print(color('                     (((|)))','orange'))
+        print(color('                    (((|)))','orange'))
+        print(color('                     (((|)))','orange'))
+        print(color('                     ((()))','orange'))
+        print(color('                       ()','orange'))
+        print('')
+        print(color(' __     __  _____  ______   ___    _____  _____','rainbow'))
+        print(color('|  |   |  ||  ___||__  __|/  _  \\ |  ___||  ___|','rainbow'))
+        print(color('|  |__ |  ||  __|   |  | |  | |  ||  __| |  __|','rainbow'))
+        print(color('|_____||__||__|     |__|  \\ ___ / |__|   |__|','rainbow'))
     elif count < -1 or count > 7:
-        print(color("Something's gone wrong! Please restart the game.", "red")) #this shouldn't happen
+        print(color('Something\'s gone wrong! Please restart the game.', 'red')) #this shouldn't happen
     else:
-        print("      /\\")
-        print("     /  \\")
-        print("    / /\\ \\")
-        print("   | (__) |")
+        print('      /\\')
+        print('     /  \\')
+        print('    / /\\ \\')
+        print('   | (__) |')
         if count > 0:
-            print("   |  " + color("o","red") + "   |")
+            print('   |  ' + color('o','red') + '   |  ' + format_alpha(alpha_list))
         else:
-            print("   |  o   |")
+            print('   |  o   |  ' + format_alpha(alpha_list))
 
         if count > 1:
-            print("   |   " + color("o","red") + "  |  ACCESS CODE: " + word_line)
+            print('   |   ' + color('o','red') + '  |  ACCESS CODE: ' + word_line)
         else:
-            print("   |   o  |  ACCESS CODE: " + word_line)
+            print('   |   o  |  ACCESS CODE: ' + word_line)
 
         if count > 2:
-            print("   |  " + color("o","red") + "   |  ATTEMPTS REMAINING: " + str(7-count) + "/7")
+            print('   |  ' + color('o','red') + '   |  ATTEMPTS REMAINING: ' + str(7-count) + '/7')
         else:
-            print("   |  o   |  ATTEMPTS REMAINING: " + str(7-count) + "/7")
+            print('   |  o   |  ATTEMPTS REMAINING: ' + str(7-count) + '/7')
 
         if count > 3:
-            print("   |   " + color("o","red") + "  |")
+            print('   |   ' + color('o','red') + '  |')
         else:
-            print("   |   o  |")
+            print('   |   o  |')
 
         if count > 4:
-            print("  /|  " + color("o","red") + "   |\\")
+            print('  /|  ' + color('o','red') + '   |\\')
         else:
-            print("  /|  o   |\\")
+            print('  /|  o   |\\')
 
         if count > 5:
-            print(" / |   " + color("o","red") + "  | \\")
+            print(' / |   ' + color('o','red') + '  | \\')
         else:
-            print(" / |   o  | \\")
+            print(' / |   o  | \\')
 
         if count > 6:
-            print("|  |  " + color("o","red") + "   |  |")
+            print('|  |  ' + color('o','red') + '   |  |')
         else:
-            print("|  |  o   |  |")
+            print('|  |  o   |  |')
 
-        print("| /|______|\\ |")
-        print("|/    \\/    \\|")
+        print('| /|______|\\ |')
+        print('|/    \\/    \\|')
+
+
+def update_alpha(alpha_list, target, clr):
+    alpha_list[alpha_list.index(target)] = color(target, clr)
+    return alpha_list
+
+
+def format_alpha(alpha_list):
+    rtn = ""
+    for elem in alpha_list:
+        rtn += elem
+        rtn += ' '
+    return rtn.strip()
 
 
 def spaceman(secret_word):
     length = len(secret_word)
     guess_list = []
+
+    f = open('alphabet.txt', 'r')
+    alpha_list = f.readlines()
+    f.close()
+    alpha_list = alpha_list[0].split(' ')
+
     wrong_count = 0
-    print("Get to space by correctly guessing letters in the rocket's launch code!\nBe careful though, you only get " + color("7","red") + " incorrect guesses before you're locked out.")
-    print("The launch code has: " + str(length) + " letters.")
+    print('Get to space by correctly guessing letters in the rocket\'s launch code!\nBe careful though, you only get ' + color('7','red') + ' incorrect guesses before you\'re locked out.')
+    print('The launch code has: ' + str(length) + ' letters.')
     page_break()
     print(secret_word)
     while is_word_guessed(secret_word, guess_list)==False and wrong_count < 7:
-        print_ship(wrong_count, get_guessed_word(secret_word, guess_list))
-        guess = get_guess("Enter a letter: ", guess_list)
+        print_ship(wrong_count, get_guessed_word(secret_word, guess_list),alpha_list)
+        guess = get_guess('Enter a letter: ', guess_list)
         guess_list.append(guess)
         if is_guess_in_word(guess, secret_word):
-            print(color("That letter is in the launch code!", "green"))
+            print(color('That letter is in the launch code!', 'green'))
+            alpha_list = update_alpha(alpha_list, guess, 'green')
         else:
-            print(color("Oops! That letter is not in the launch code.", "red"))
+            print(color('Oops! That letter is not in the launch code.', 'red'))
+            alpha_list = update_alpha(alpha_list, guess, 'red')
             wrong_count += 1
         page_break()
     if wrong_count >= 7:
-        print_ship(7, get_guessed_word(secret_word, guess_list))
-        print(color("ACCESS DENIED. The code was " + secret_word, "red"))
+        print_ship(7, get_guessed_word(secret_word, guess_list), alpha_list)
+        print(color('ACCESS DENIED. The code was ' + secret_word, 'red'))
     else:
-        print(color("ACCESS GRANTED. The code was " + secret_word, "green"))
-        print_ship(-1, "")
+        print(color('ACCESS GRANTED. The code was ' + secret_word, 'green'))
+        print_ship(-1, '', alpha_list)
 
 
 #These function calls that will start the game
